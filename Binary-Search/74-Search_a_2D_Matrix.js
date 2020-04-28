5876f764b9ad26b926c0d5e47f1073e4a2de8552
q15517110516 <lmrzds@gmail.com>
2020-04-28 17:30:13 -0400

    --- Auto-Git Commit ---

--- Binary-Search/74-Search_a_2D_Matrix.js
+++ Binary-Search/74-Search_a_2D_Matrix.js
@@ -32,64 +32,119 @@
  */
 var searchMatrix = function(matrix, target) {
 
-    // Method 1
-    // if(matrix.length === 0){
-    //     return false;
-    // }
-    // let start = 0;
-    // let end = matrix.length - 1;
-    // let innerLen = matrix[0].length;
-    // let innerLast = innerLen - 1;
-
-    // while(start + 1 < end){
-    //     let mid = parseInt(start + (end - start)/2);
-    //     if(matrix[mid][innerLast] === target){
-    //         return true;
-    //     }
-    //     else if(matrix[mid][innerLast] < target){
-    //         start = mid;
-    //     }
-    //     else if(matrix[mid][innerLast] > target){
-    //         end = mid;
-    //     }
-    // }
-
-    // Method 2
+    // Method 1: Using Binary Search to filter the last 2 arrays
     if(matrix.length === 0){
         return false;
     }
-
-    let res = [];
-
-    for(let i = 0; i < matrix.length; i++){
-        res = res.concat(matrix[i]);
-    }
-
     let start = 0;
-    let end = res.length;
+    let end = matrix.length - 1;
+    let innerLen = matrix[0].length;
+    let innerLast = innerLen - 1;
 
     while(start + 1 < end){
         let mid = parseInt(start + (end - start)/2);
-        if(res[mid] === target){
+        if(matrix[mid][innerLast] === target){
             return true;
         }
-        else if(res[mid] < target){
+        else if(matrix[mid][innerLast] < target){
             start = mid;
         }
-        else{
+        else if(matrix[mid][innerLast] > target){
             end = mid;
         }
     }
 
+    // Inner array start & end
+    let start1 = 0;
+    let end1 = matrix[start].length;
+    let end2 = matrix[end].length;
 
-    if(res[start] === target){
-        return true;
+    if(matrix[start][innerLast] < target){
+        while(start1 + 1 < end1){
+            mid = parseInt(start1 + (end1 - start1)/2);
+            if(matrix[mid] === target){
+                return true;
+            }
+            else if(matrix[mid] < target){
+                start1 = mid;
+            }
+            else{
+                end1 = mid;
+            }
+        }
+
+        if(matrix[start1] = target){
+            return true;
+        }
+        if(matrix[end1] = target){
+            return true;
+        }
+        return false;
+
     }
-    if(res[end] === target){
-        return true;
+
+    start1 = 0;
+    if(matrix[start][innerLast] > target){
+        while(start1 + 1 < end2){
+            mid = parseInt(start1 + (end2 - start1)/2);
+            if(matrix[mid] === target){
+                return true;
+            }
+            else if(matrix[mid] < target){
+                start1 = mid;
+            }
+            else{
+                end2 = mid;
+            }
+        }
+
+        if(matrix[start1] = target){
+            return true;
+        }
+        if(matrix[end1] = target){
+            return true;
+        }
+        return false;
     }
 
     return false;
+
+    // Method 2: Using 'concat' to combine all the arrays into one array, then use Binary Search
+    // if(matrix.length === 0){
+    //     return false;
+    // }
+
+    // let res = [];
+
+    // for(let i = 0; i < matrix.length; i++){
+    //     res = res.concat(matrix[i]);
+    // }
+
+    // let start = 0;
+    // let end = res.length;
+
+    // while(start + 1 < end){
+    //     let mid = parseInt(start + (end - start)/2);
+    //     if(res[mid] === target){
+    //         return true;
+    //     }
+    //     else if(res[mid] < target){
+    //         start = mid;
+    //     }
+    //     else{
+    //         end = mid;
+    //     }
+    // }
+
+
+    // if(res[start] === target){
+    //     return true;
+    // }
+    // if(res[end] === target){
+    //     return true;
+    // }
+
+    // return false;
 
     
 
