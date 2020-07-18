8c363ec30b494fd51ff16d1167856fc569496724
q15517110516 <lmrzds@gmail.com>
2020-07-18 16:17:01 -0400

    441. Arranging Coins

---
+++ Binary-Search/441-Arranging_Coins.js
@@ -0,0 +1,55 @@
+// You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+// Given n, find the total number of full staircase rows that can be formed.
+
+// n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+// Example 1:
+
+// n = 5
+
+// The coins can form the following rows:
+// ¤
+// ¤ ¤
+// ¤ ¤
+
+// Because the 3rd row is incomplete, we return 2.
+// Example 2:
+
+// n = 8
+
+// The coins can form the following rows:
+// ¤
+// ¤ ¤
+// ¤ ¤ ¤
+// ¤ ¤
+
+// Because the 4th row is incomplete, we return 3.
+
+
+// 
+// @param {number} n
+// @return {number}
+// 
+var arrangeCoins = function(n) {
+    
+    let start = 0;
+    let end = n;
+
+    while(start <= end){
+
+        let mid = parseInt(start + (end - start)/2);
+        let sum = mid * (mid + 1)/2;
+
+        if(sum === n){
+            return mid;
+        }
+        else if(sum > n){
+            end = mid - 1;
+        }
+        else if(sum < n){
+            start = mid + 1;
+        }
+    }
+    return end;
+};
