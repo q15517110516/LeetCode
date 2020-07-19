fc77d0a8dc2e8046c45fafddbda3fc46e2d0905a
q15517110516 <lmrzds@gmail.com>
2020-07-19 15:47:58 -0400

    33. Search in Rotated Sorted Array

--- Binary-Search/33-Search_in_Rotated_Sorted_Array.js
+++ Binary-Search/33-Search_in_Rotated_Sorted_Array.js
@@ -21,4 +21,40 @@
  */
 var search = function(nums, target) {
     
+    let start = 0;
+    let end = nums.length - 1;
+
+    while(start + 1 < end){
+
+        let mid = parseInt(start + (end - start)/2);
+        if(nums[mid] === target){
+            return mid;
+        }
+        else if(nums[mid] > nums[start]){
+            if(target >= nums[start] && target <= nums[mid]){
+                end = mid;
+            }
+            else{
+                start = mid;
+            }
+
+        }
+        else{
+            if(nums[mid] <= target && target <= nums[end]){
+                start = mid;
+            }
+            else{
+                end = mid;
+            }
+            
+        }
+    }
+
+    if(nums[start] === target){
+        return start;
+    }
+    if(nums[end] === target){
+        return end;
+    }
+    return -1;
 };
