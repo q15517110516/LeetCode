767e049f650c717eb705e57b5f9f58f848e6b51a
q15517110516 <lmrzds@gmail.com>
2020-07-19 20:44:08 -0400

    81. Search in Rotated Sorted Array II

--- Binary-Search/81-Search_in_Rotated_Sorted_ArrayII.js
+++ Binary-Search/81-Search_in_Rotated_Sorted_ArrayII.js
@@ -24,4 +24,11 @@
  */
 var search = function(nums, target) {
     
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === target){
+            return true;
+        }
+    }
+    return false;
+
 };
