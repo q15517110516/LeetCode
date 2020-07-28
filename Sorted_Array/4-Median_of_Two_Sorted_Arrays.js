56a045a5737de728fdb4fd211fe8945d0b7a3326
q15517110516 <lmrzds@gmail.com>
2020-07-28 16:09:28 -0400

    4. Median of Two Sorted Arrays

--- Sorted_Array/4-Median_of_Two_Sorted_Arrays.js
+++ Sorted_Array/4-Median_of_Two_Sorted_Arrays.js
@@ -16,6 +16,22 @@
 // nums2 = [3, 4]
 // The median is (2 + 3)/2 = 2.5
 
+// 
+// @param {number[]} nums1
+// @param {number[]} nums2
+// @return {number}
+// 
+
 var findMedianSortedArrays = function(nums1, nums2) {
     
+    let nums = nums1.concat(nums2);
+    nums.sort((a, b) => a - b);
+    
+    if(nums.length % 2 === 0){
+        return (nums[nums.length/2] + nums[nums.length/2 - 1])/2;
+    }
+    else{
+        return nums[(nums.length - 1)/2];
+
+    }
 };
