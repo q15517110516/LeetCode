cb409e658f40309e23d60805858ed7585d2692f4
q15517110516 <lmrzds@gmail.com>
2020-08-31 15:50:23 -0400

    26. Remove Duplicate from Sorted Array

--- Sorted_Array/26-Remove_Duplicates_from_Sorted_Array.js
+++ Sorted_Array/26-Remove_Duplicates_from_Sorted_Array.js
@@ -22,12 +22,22 @@
 // @return {number}
 // 
 var removeDuplicates = function(nums) {
+
+    // Two Pointers
     for(let i = 0; i < nums.length - 1; i++){
         for(let j = i + 1; j < nums.length; j++){
             if(nums[i] === nums[j]){
                 nums.splice(i, 1);
                 i--;
             }
+        }
+    }
+    return nums.length;
+
+    // One Pointer
+    for(let i = nums.length - 1; i > 0; i--){
+        if(nums[i] === nums[i-1]){
+            nums.splice(i, 1);
         }
     }
     return nums.length;
